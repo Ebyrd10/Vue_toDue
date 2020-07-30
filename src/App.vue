93a1d64734df363a78262532ff1d1930b3ecd45a
Ethan <Ethanmbyrd@gmail.com>
2020-07-29 23:11:52 -0400

    adding add and delete functionality

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
         class='input'
         v-model="newToDo"
         />
-        <button class="add-button">Add</button>
+        <button class="add-button" v-on:click="add()">Add</button>
         <ul style="list-styke-type: none;">
           <li v-for="(todo, i) in existingToDo"> 
             <span> 
               {{i + 1}}: {{todo.text}}
             </span>
+            <button class="delete-button" @click="deleteToDo(i)"> 
+              X
+              </button>
           </li>
         </ul>
     </div>
@@ -29,6 +32,18 @@ export default {
         {text: 'run errrands', id:1},
         {text: 'walking dog', id:2}
       ]
+    }
+  },
+  methods:{
+    add(){
+      this.existingToDo.push({
+        text: this.newToDo,
+        id: new Date().valueOf()
+      }),
+      this.newToDo=''
+    },
+    deleteToDo(i){
+      this.existingToDo.splice(i,1)
     }
   }
 }
