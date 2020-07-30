2319f765ff28b726b980cb646c9aa0682e301e8f
Ethan <Ethanmbyrd@gmail.com>
2020-07-29 22:25:58 -0400

    connecting data to front-end view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+        <h1> VuetoDue </h1>
+        <input
+        class='input'
+        v-model="newToDo"
+        />
+        <button class="add-button">Add</button>
+        <ul style="list-styke-type: none;">
+          <li v-for="(todo, i) in existingToDo"> 
+            <span> 
+              {{i + 1}}: {{todo.text}}
+            </span>
+          </li>
+        </ul>
+    </div>
   </div>
 </template>
 
@@ -9,7 +23,12 @@ export default {
   name: 'app',
   data () {
     return {
-      
+      newToDo: '',
+      existingToDo: [
+        {text: 'get groceries', id:0},
+        {text: 'run errrands', id:1},
+        {text: 'walking dog', id:2}
+      ]
     }
   }
 }
